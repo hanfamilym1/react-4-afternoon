2ff5ba9425e965785d929c0ac20d10eee3949cfe
hanfamilym1 <michael@hanfamily.com>
2018-08-28 15:45:42 -0600

    Finished Afternoon Project

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import {Link, Switch, Route} from 'react-router-dom'
+import History from '../History/History'
+import Contact from '../Contact/Contact'
+
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+            <Link to='/about' className='subnav_links'><h3>About</h3></Link>
+            <Link to='/about/history' className='subnav_links'><h3>History</h3></Link>
+            <Link to='/about/contact' className='subnav_links'><h3>Contact</h3></Link>
         </div>
         <div className='box'>
+          <Switch>
+            <Route path='/about/history' component={History}/>
+            <Route path='/about/contact' component={Contact}/>
+            <Route exact path='/about' render={()=>(
+              <div>
+                <h1>About the University</h1>
+                <p>Every day, people join our university as a sanctuary to find who they are and what they represent. WestSide University welcomes all students who are willing to push themselves and ready to make an effort to not only change themselves, but the world.</p>
+              </div>
+            )}/>
+          </Switch>
         </div>
       </div>
     )
   }
 }
+            
+
+
+
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state={
+      students: []
+    }
     
   }
 
+  componentDidMount(){
+    console.log('props', this.props.match.params.class)
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then((res)=>
+    this.setState({students: res.data})
+    // console.log('students', res.data)
+  
+  )
+  }
+
   render() {
+    
+    let newStudents = this.state.students.map((student, i)=> <Link to={`/student/${student.id}`} key={i}><h3 >{student.first_name} {student.last_name}</h3></Link>)
+    
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
-
+        {newStudents}
+        <Link to='/'> <button>Go Home</button> </Link>
       </div>
     )
   }
